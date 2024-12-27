67c76fee920018cf98a182eb6d03227c1254e1b4
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-12-27 16:42:44 +0500

    update hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,78 +1,50 @@
-'use client';
-import Link from 'next/link';
-import Image from 'next/image';
+import Image from "next/image";
 
-
-function Hero() {
- 
-
+const Hero = () => {
   return (
-    <div className="relative w-full bg-[#F3F3F3] overflow-hidden flex flex-col md:flex-row items-center justify-between">
+    <div className="bg-slate-100 py-12">
+      <div className="max-w-7xl mx-auto flex flex-col items-center lg:items-start lg:flex-row justify-between px-6 sm:px-10">
+        
+   
+        <div className="w-full lg:w-1/3 mb-8 lg:mb-0 flex justify-center lg:justify-start "> 
+          <Image
+            src="/images/image 32.png"
+            alt="Left Image"
+            width={300}
+            height={300}
+            className="-mt-12"
+          />
+        </div>
 
-      {/* Left Section for Mobile & Desktop */}
-      <div className="w-full md:w-1/2  h-full flex flex-col md:flex-row items-center justify-start pl-6 md:pl-16 relative ">
-
-        {/* Bulb Image */}
-<div className="relative md:absolute top-[-20px] left-[-20px] md:left-[-80px]">
-  <Image src="/images/bulb.png" alt="Bulb" width={300} height={300} className="object-contain" />
-</div>
-
-
-        {/* Text Content - Aligning next to the bulb */}
-        <div className="relative mt-6 md:mt-0 md:ml-6 text-center md:text-left">
-          {/* Pink Text */}
-          <p className="text-[#FB2E86] text-lg md:text-xl font-normal mb-2">
-            Best Furniture For Your Castle...
+        <div className="lg:w-2/3 lg:pl-0 mt-6 lg:mt-0 text-left"> 
+          <p className="text-pink-500 text-sm mt-2"> 
+            Best Furniture for Your Castle
           </p>
-
-          {/* Bold Heading */}
-          <h1 className="text-4xl md:text-4xl font-bold text-black leading-tight mb-4">
-            New Furniture Collection Trends in 2020
+          <h1 className="text-4xl font-bold text-gray-900 mt-4 leading-tight">
+            New Furniture Collection <br />
+            Trends in 2020
           </h1>
-
-          {/* Description */}
-          <p className="text-left text-sm md:text-lg text-gray-600 mb-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing in
-            phasellus non in justo.
+          <p className="text-blue-700 mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> Sed
+            ullamcorper, velit at feugiat ultricies.
           </p>
-
-          {/* Shop Now Button */}
-          <Link href="/shoplist">
-            <button className="px-4 py-2 bg-[#FB2E86] text-white font-text rounded-md shadow-md hover:bg-pink-600 justify-center">
-              Shop Now
-            </button>
-          </Link>
+          <button className="bg-pink-600 text-white text-sm font-semibold px-6 py-3 rounded-full mt-6 hover:bg-pink-500">
+            Shop Now
+          </button>
         </div>
 
-      </div>
-
-      {/* Right Section (Desktop, Mobile - Scrollable) */}
-      <div className="w-full md:w-1/2 h-full relative flex flex-col items-center justify-start">
-
-        {/* Pink Background Image */}
-        <div
-          className="absolute top-[60px] w-[380px] md:w-[450px] h-[380px] md:h-[450px] bg-cover bg-center z-0"
-          style={{ backgroundImage: 'url(/images/pink.png)' }}
-
-        ></div>
-
-        {/* Sofa Image */}
-        <div className="relative z-10 mt-[80px] mb-[20px]">
-          <Image src="/images/sofa.png" alt="Sofa" width={350} height={250} className="object-contain" />
-        </div>
-
-        {/* Blue Discount Badge */}
-        <div
-          className="absolute top-[40px] right-[75px] bg-cover bg-center w-[80px] h-[80px] flex flex-col justify-center items-center z-20"
-          style={{ backgroundImage: 'url(/images/discount.png)' }}
-        >
-          <span className="text-xl font-bold text-white">50%</span>
-          <span className="text-sm font-semibold text-white">OFF</span>
+        <div className="w-full lg:w-1/3 flex justify-center lg:justify-end mt-6"> 
+          <Image
+            src="/images/sofa promotional header.png"
+            alt="Right Image"
+            width={700}
+            height={600}
+            className="rounded-lg"
+          />
         </div>
       </div>
-
     </div>
   );
-}
+};
 
 export default Hero;
